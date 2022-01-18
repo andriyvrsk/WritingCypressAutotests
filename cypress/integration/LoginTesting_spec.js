0b63dbe0721ab67bb6d18b5c439ac62cd7b5b39f
Andrii Yavorskyi (I.) <andrii.i.yavorskyi@dataart.com>
2022-01-18 18:08:05 +0200

    point3

---
+++ cypress/integration/LoginTesting_spec.js
@@ -0,0 +1,13 @@
+const { describe } = require("mocha");
+
+describe('myTest', () => {
+  it('diffScreenResolutions', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/');
+    cy.viewport(1280, 1024);
+    cy.wait(8000);
+    cy.viewport(768, 1024);
+    cy.wait(8000);
+    cy.viewport(414, 896);
+  })
+})
+
