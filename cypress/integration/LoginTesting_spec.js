836155875975c1e9b471b3525d74a6210a68e8d8
Andrii Yavorskyi <andriyvrsk@gmail.com>
2022-01-19 14:27:46 +0200

    Added two functions

--- cypress/integration/LoginTesting_spec.js
+++ cypress/integration/LoginTesting_spec.js
@@ -10,20 +10,22 @@ let submitButtonLocator1 = '#btnLogin';
 let submitButtonLocator2 = '[name="Submit"]';
 let submitButtonLocator3 = '.button';
 
-describe('different screen resolutions', () => {
-  it('diffScreenResolutions', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/');
-    cy.viewport(1280, 1024);
-    cy.wait(8000);
-    cy.viewport(768, 1024);
-    cy.wait(8000);
-    cy.viewport(414, 896 ); 
-  })
-})
+// describe('different screen resolutions', () => {
+//   it('diffScreenResolutions', () => {
+//     cy.visit('https://opensource-demo.orangehrmlive.com/');
+//     //navigateTo();
+//     cy.viewport(1280, 1024);
+//     cy.wait(8000);
+//     cy.viewport(768, 1024);
+//     cy.wait(8000);
+//     cy.viewport(414, 896 ); 
+//   })
+// })
 
 describe('positive login check', () => {
   it('loginCheckPos', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/');
+    //navigateTo();
     cy.get(usernameLocator1).type('Admin');
     cy.get(passwordLocator1).type('admin123');
     cy.get(submitButtonLocator3).click();
@@ -40,6 +42,7 @@ describe('negative login check', () => {
     cy.get(usernameLocator1).type('Admin');
     cy.get(passwordLocator1).type('12345');
     cy.get(submitButtonLocator3).click();
+    //logIn('Admin', '123');
     cy.get('[id="spanMessage"]').should('contain', 'Invalid credentials');
   })
 })
@@ -53,3 +56,13 @@ describe('negative login check', () => {
     cy.get('[id="spanMessage"]').should('contain', 'Invalid credentials');
   })
 })
+
+function navigateTo (url = 'https://opensource-demo.orangehrmlive.com/') {
+  cy.visit(url);
+}
+
+function logIn(username = 'Admin', password = 'admin123') {
+  cy.get(usernameLocator1).type(username);
+  cy.get(passwordLocator1).type(password);
+  cy.get(submitButtonLocator3).click();
+}
